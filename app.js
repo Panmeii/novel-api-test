974b69cc3c1a2805c6041fae6a441d9e6ea3c49e
Panmeii <134782372+Panmeii@users.noreply.github.com>
2025-03-26 13:25:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const esnovelRoutes = require("./src/novels/routes/esnovelsRoutes");
 const fanTranslationRoutes = require("./src/novels/routes/fantranslationRoute");
+const bacalightRoutes = require("./src/novels/routes/bacalightRoute");
 
 const app = express();
 const port = 5000;
@@ -11,8 +12,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to Jellee Api - your destination for scrapping data from various novel & manga sites! Seamlessly access  id, title, image and more.");
 });
 
-app.use("/novel", esnovelRoutes);
+app.use("/esnovel", esnovelRoutes);
 app.use("/novel", fanTranslationRoutes);
+app.use("/bacalight", bacalightRoutes);
+
 
 // Start server
 app.listen(port, () => {
