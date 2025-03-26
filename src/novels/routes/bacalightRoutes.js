56584275d944930c416f176cd96f0aa626911490
Panmeii <134782372+Panmeii@users.noreply.github.com>
2025-03-26 11:58:42 +0700

    Update bacalightRoutes.js

--- src/novels/routes/bacalightRoutes.js
+++ src/novels/routes/bacalightRoutes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const {
   getLatestChapter,
-  
+  getFinishedNovels,
+  getTrendingNovels,
+  getMostViewedNovels,
+  getNewestNovels,
+  getMostRatedNovels,
+  getAllNovels
 } = require("../controllers/bacalightController");
 
 const router = express.Router();
 
 router.get("/bacalight/latest", getLatestChapter);
+router.get("/bacalight/finished", getFinishedNovels);
+router.get("/bacalight/trending", getTrendingNovels);
+router.get("/bacalight/most-viewed", getMostViewedNovels);
+router.get("/bacalight/newest", getNewestNovels);
+router.get("/bacalight/most-rated", getMostRatedNovels);
+router.get("/bacalight/all", getAllNovels);
 
 module.exports = router;
